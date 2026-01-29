2ab0974d5a19ccc2ab8e9c2f3f3b813d9c905684
Sohaib0403 <sohaibsiddiqui0304@gmail.com>
2026-01-29 10:55:14 +0530

    learn about components

---
+++ second-project/src/App.jsx
@@ -0,0 +1,19 @@
+function App(){
+  return(
+    <>
+    <h1>first componen</h1>
+    <p>second component : </p>
+    <Name/>
+    </>
+  )
+}
+
+function Name(){
+  return(
+    <>
+    <h2>Sohaib Siddiqui</h2>
+    </>
+  )
+}
+
+export default App
