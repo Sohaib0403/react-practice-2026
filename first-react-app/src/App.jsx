d64b99b12c84dab28cd605aa9abd8087880ed2a4
Sohaib0403 <sohaibsiddiqui0304@gmail.com>
2026-01-29 10:01:05 +0530

    added first react demo

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Home"
+
+function app(){
+  return(
+    <>
+    <Header/>
+    <h1>sohaib siddiqui</h1>
+    <h2>my first project</h2>
+    </>
+  )
+}
+
+export default app
---
+++ first-react-app/src/Home.jsx
@@ -0,0 +1,9 @@
+function Header(){
+return(
+    <>
+    <h3>its my new component</h3>
+    </>
+)
+}
+
+export default Header
